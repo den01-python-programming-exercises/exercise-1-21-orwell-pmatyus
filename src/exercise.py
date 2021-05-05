365c7e7b2cd083eddc1a01fb2d82b7cc08702c22
pmatyus <>
2021-05-05 15:03:27 +0000

    Remember to add colon after if statement

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
+    number = int(input("Give a number:"))
+    if (number == 1984):
+        print("Orwell")
 
 if __name__ == '__main__':
     main()
